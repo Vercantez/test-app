43209b17dd21c2ab36ded7f8a32a4df62d9b7998
Miguel Salinas <mikeys@me.com>
2024-05-03 21:18:09 -0700

    Initial Commit

---
+++ test-app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test-app
+//
+//  Created by Miguel Salinas on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
